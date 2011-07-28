1629fb24d75fe5febc7f264b3bdc0cddbeb8e0bc
christian <christianessl@googlemail.com>
2011-07-28 16:19:51 +0200

    added no full yeti support

---
+++ src/it/testNoYetiOnPathUseLib/validate.groovy
@@ -0,0 +1,19 @@
+try {
+
+def file = new File(basedir, 'target/classes/fb/foo.class')
+assert file.exists()
+
+def file2 = new File(basedir, 'target/classes/fb2/foo2.class')
+assert file2.exists()
+
+def file3 = new File(basedir, 'target/classes/TestClass.class')
+assert file3.exists()
+
+
+
+return true
+
+} catch(Throwable e) {
+  e.printStackTrace()
+  return false
+}
